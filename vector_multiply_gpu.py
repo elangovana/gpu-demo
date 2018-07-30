3b291e3155429bae24d6ccd83d9922396f22610e
aeg <aeg@amazon.com>
2018-07-30 15:16:32 +1000

    Add samples

--- vector_multiply_gpu.py
+++ vector_multiply_gpu.py
@@ -17,17 +17,17 @@ from numba import vectorize
 # Parallelise multiply operations
 @vectorize("float32(float32,float32,float32,float32)", target='cuda')
 def mutiply(a, b, c, d):
-    return (a * (b ** 10) * c * (d ** 240) * ((a + b) ** 9) * ((c + d) ** 100) * ((a + d) ** 10))**100
+    return  (a * (b ** 100) ** c * (d ** 240) * ((a + b) ** 9) * ((c + d) ** 100) * ((a + d) ** 10))**100
 
 
 def main(vector_length):
     start_time = datetime.now()
 
     # Initialise vectors, each with length N with ones of float type
-    A = np.ones(vector_length).astype(np.float32)
-    B = np.ones(vector_length).astype(np.float32)
-    C = np.ones(vector_length).astype(np.float32)
-    D = np.ones(vector_length).astype(np.float32)
+    A = np.ones(vector_length).astype(np.float32) * 3
+    B = np.ones(vector_length).astype(np.float32) * 4
+    C = np.ones(vector_length).astype(np.float32) * 4
+    D = np.ones(vector_length).astype(np.float32) * 5
 
     # Compute vector multiply
     exec_start_time = datetime.now()
