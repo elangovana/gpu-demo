e5dfd4adc786d2e617d80f33b9524d0aed922d11
aeg <aeg@amazon.com>
2018-08-02 20:38:39 +1000

    Add samples

--- demo2_s3_download_speed/transfer_s3.py
+++ demo2_s3_download_speed/transfer_s3.py
@@ -23,11 +23,11 @@ def downloadfile(s3bucket, s3key, saveto_localpath):
 
     config = TransferConfig(
         # The transfer size threshold for which multipart uploads, downloads, and copies will automatically be triggereds
-        multipart_threshold=100 * MB,
+        multipart_threshold=126 * MB,
         # The maximum number of threads that will be making requests to perform a transfer
         max_concurrency=10,
         # The partition size of each part for a multipart transfer
-        multipart_chunksize=100 * MB,
+        multipart_chunksize=126 * MB,
         # The maximum amount of read parts that can be queued in memory to be written for a download
         max_io_queue=126,
         #  The max size of each chunk in the io queue
